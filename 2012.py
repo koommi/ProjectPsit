2a5b5e988c38ef695e660d2ab179e7831c0ff77e
Kannaphat Khunakornpattanakarn <beobonus@hotmail.com>
2015-12-11 19:46:20 +0700

    graph

---
+++ 2012.py
@@ -0,0 +1,80 @@
+import plotly.plotly as py
+from plotly.graph_objs import *
+py.sign_in('beobonus', '02p6asccbu')
+trace1 = Bar(
+    x=['East Asia'],
+    y=[' 12,525,214 '],
+    name='East Asia',
+    uid='ac9ba3',
+    xsrc='beobonus:88:01908d?01908d="East Asia"',
+    ysrc='beobonus:88:b633ff?01908d="East Asia"'
+)
+trace2 = Bar(
+    x=['Europe'],
+    y=[' 5,650,619 '],
+    name='Europe',
+    uid='81d6be',
+    xsrc='beobonus:88:01908d?01908d="Europe"',
+    ysrc='beobonus:88:b633ff?01908d="Europe"'
+)
+trace3 = Bar(
+    x=['Amercica'],
+    y=[' 1,083,433 '],
+    name='Amercica',
+    uid='094937',
+    xsrc='beobonus:88:01908d?01908d="Amercica"',
+    ysrc='beobonus:88:b633ff?01908d="Amercica"'
+)
+trace4 = Bar(
+    x=['South Asia'],
+    y=[' 1,286,861 '],
+    name='South Asia',
+    uid='a0968c',
+    xsrc='beobonus:88:01908d?01908d="South Asia"',
+    ysrc='beobonus:88:b633ff?01908d="South Asia"'
+)
+trace5 = Bar(
+    x=['Oceania'],
+    y=[' 1,046,755 '],
+    name='Oceania',
+    uid='c77e45',
+    xsrc='beobonus:88:01908d?01908d="Oceania"',
+    ysrc='beobonus:88:b633ff?01908d="Oceania"'
+)
+trace6 = Bar(
+    x=['Middle East'],
+    y=[' 605,477 '],
+    name='Middle East',
+    uid='282db6',
+    xsrc='beobonus:88:01908d?01908d="Middle East"',
+    ysrc='beobonus:88:b633ff?01908d="Middle East"'
+)
+trace7 = Bar(
+    x=['Africa'],
+    y=[' 155,544 '],
+    name='Africa',
+    uid='d7caa2',
+    xsrc='beobonus:88:01908d?01908d="Africa"',
+    ysrc='beobonus:88:b633ff?01908d="Africa"'
+)
+data = Data([trace1, trace2, trace3, trace4, trace5, trace6, trace7])
+layout = Layout(
+    autosize=True,
+    barmode='group',
+    height=514,
+    title='Number of foreign tourists in other continent.',
+    width=1121,
+    xaxis=XAxis(
+        autorange=True,
+        range=[-0.5, 6.5],
+        title='Name of continent.',
+        type='category'
+    ),
+    yaxis=YAxis(
+        autorange=True,
+        range=[0, 13184435.789473685],
+        title='Number of people.'
+    )
+)
+fig = Figure(data=data, layout=layout)
+plot_url = py.plot(fig)
